8c3c32b672f4deb8e2fafae60a14d5787e462ad3
Laurent Paumier <laurent.paumier@fnac.net>
2022-04-22 08:10:41 +0000

    test person

--- src/main/java/fr/upshift/demo/resource/PersonResource.java
+++ src/main/java/fr/upshift/demo/resource/PersonResource.java
@@ -1,10 +1,9 @@
 package fr.upshift.demo.resource;
 
 import java.net.URI;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,35 +12,35 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import io.quarkus.logging.Log;
+import io.smallrye.common.annotation.NonBlocking;
+
 import org.jboss.resteasy.reactive.RestQuery;
 
 import fr.upshift.demo.entity.Person;
-import fr.upshift.demo.entity.Status;
-import io.quarkus.logging.Log;
-import io.smallrye.common.annotation.NonBlocking;
+import fr.upshift.demo.service.PersonService;
 
 @Path("/person")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PersonResource {
 
+    @Inject
+    PersonService personService;
+
     @GET
     @Path("/init")
     @NonBlocking
     public void init() {
         Log.info("Init");
-        Person person = new Person();
-        person.name = "Laurent";
-        person.birth = LocalDate.of(1971, Month.NOVEMBER, 4);
-        person.status = Status.LIVING;
-        person.persist();
+        personService.init();
     }
 
     @GET
     @NonBlocking
     public List<Person> list() {
         Log.info("List persons");
-        return Person.listAll();
+        return personService.list();
     }
 
     @GET
@@ -49,14 +48,14 @@ public class PersonResource {
     @NonBlocking
     public Person get(@RestQuery String name) {
         Log.info("Find person " + name);
-        return Person.find("name", name).firstResult();
+        return personService.get(name);
     }
 
     @POST
     @NonBlocking
     public Response create(Person person) {
         Log.info("Create person " + person.name);
-        person.persist();
+        personService.create(person);
         return Response.created(URI.create("/person/" + person.id)).build();
     }
 }
